0a863bc4afadce08cdce601af7a98e0f5ba1ca68
Florian Witteler <Florian Witteler github@flwi.de>
2017-02-18 14:34:42 +0100

    built own index.html. elm-make now creates elm-main.js

--- build-github-page.sh
+++ build-github-page.sh
@@ -1,10 +1,10 @@
-elm-make MagnetPlayground.elm --output docs/index.html
+elm-make MagnetPlayground.elm --output docs/elm-main.js
 
-git add docs/index.html
+git add docs/elmMain.js
 
 echo
 echo "added updated html file for github-pages"
 echo "run these commands to publish"
 
-echo "git commit -m \"updated github-pages index.html\""
+echo "git commit -m \"updated github-pages elm-main.js\""
 echo "git push"
