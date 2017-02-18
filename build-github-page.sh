3b0dca90a8c1a351c94a001b0aed3378972806eb
Florian Witteler <Florian Witteler github@flwi.de>
2017-02-18 15:10:27 +0100

    minifying js using google's clojure compiler

--- build-github-page.sh
+++ build-github-page.sh
@@ -1,10 +1,21 @@
-elm-make MagnetPlayground.elm --output docs/elm-main.js
+CWD="$(PWD)"
+DOCS_DIR="$CWD/docs"
 
-git add docs/elmMain.js
+ELM_FILE="$CWD/MagnetPlayground.elm"
+JS_FILE="$CWD/elm-main.js"
+JS_MIN_FILE="$CWD/docs/elm-main.min.js"
+
+elm-make "$ELM_FILE" --output "$JS_FILE"
+
+echo "minifying js-file using the google closure compiler ..."
+google-closure-compiler-js "$JS_FILE" > "$JS_MIN_FILE"
+
+git add "$JS_MIN_FILE"
+exit 1
 
 echo
 echo "added updated html file for github-pages"
 echo "run these commands to publish"
 
-echo "git commit -m \"updated github-pages elm-main.js\""
+echo "git commit -m \"updated github-pages elm-main.min.js\""
 echo "git push"
