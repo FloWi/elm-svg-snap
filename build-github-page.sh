f80677cbded11764625981bd5503148840de7bd9
Florian Witteler <Florian Witteler github@flwi.de>
2017-02-18 13:32:59 +0100

    added poor-mans build & deploy script

---
+++ build-github-page.sh
@@ -0,0 +1,10 @@
+elm-make MagnetPlayground.elm --output docs/index.html
+
+git add docs/index.html
+
+echo
+echo "added updated html file for github-pages"
+echo "run these commands to publish"
+
+echo "git commit -m \"updated github-pages index.html\""
+echo "git push"
